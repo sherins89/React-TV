08816fd25de5c9f1ce61010ce86fbce1e5d95d72
Sherin <sherin.daniel89@yahoo.com>
2025-09-14 01:19:34 -0400

    Updated React TV'

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,6 @@ import { tvShows } from "./shows/data";
 import ShowDetails from "./shows/ShowDetails";
 import ShowSelection from "./shows/ShowSelection";
 
-/**
- * React TV is an web streaming platform that allows users to browse
- * through the episodes of a variety of different shows.
- */
 export default function App() {
   const [shows] = useState(tvShows);
   const [selectedShow, setSelectedShow] = useState();
@@ -21,11 +17,6 @@ export default function App() {
           setSelectedShow={setSelectedShow}
         />
       </header>
-
-      {/*
-        Changing the `key` prop will force the ShowDetails to rerender
-        whenever the selected show changes.
-      */}
       <main>
         <ShowDetails key={selectedShow?.name} show={selectedShow} />
       </main>
